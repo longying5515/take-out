bd0f5ebbdf25d41b4e32290d79abb4d8df3bc067
LongYing <longying5515@gmail.com>
2024-04-10 00:45:26 +0800

    菜品上传

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,25 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/dish")
+@Slf4j
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @PostMapping
+    public Result save(@RequestBody DishDTO dishDTO){
+        log.info("新增菜品：{}",dishDTO);
+        dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+}
